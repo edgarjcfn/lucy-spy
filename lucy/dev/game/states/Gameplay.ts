96fbd63041cc32cf7ccf699b99e30be9e46e85f6
edgarjcfn <edgar.figueiredo@gmail.com>
2014-12-06 15:02:11 +0100

    Fixed tiles position

--- lucy/dev/game/states/Gameplay.ts
+++ lucy/dev/game/states/Gameplay.ts
@@ -24,29 +24,37 @@ module KodingSpy {
             var columns = 16;
 
             // Floor
-            for (var x = 0; x <= columns; x++) {
-                for (var y = 0; y <= rows; y++) {
-                    var position = KodingSpy.Utils.getWorldPosition(x, y);
-                    this.game.add.sprite(position.x, position.y, 'tileFLOOR');
+            for (var x = 0; x < columns; x++) {
+                for (var y = 0; y < rows; y++) {
+                    this.placeTileAt(x, y, 'tileFLOOR');
                 }
             }
 
             // Top and Bottom walls
-            for (var x = 0; x <= columns; x++) {
-                var position = KodingSpy.Utils.getWorldPosition(x, 0);
-                this.game.add.sprite(position.x, position.y, 'tileWALL_TOP');
-                position = KodingSpy.Utils.getWorldPosition(x, rows-1);
-                this.game.add.sprite(position.x, position.y, 'tileWALL_BACK');
+            for (var x = 0; x < columns; x++) {
+                this.placeTileAt(x, 0, 'tileWALL_TOP');
+                this.placeTileAt(x, 1, 'tileWALL_FACE');
+                this.placeTileAt(x, rows-1, 'tileWALL_BACK');
             }
 
             // Left and Right walls
-            for (var y = 0; y <= rows; y++) {
-                var position = KodingSpy.Utils.getWorldPosition(0, y);
-                this.game.add.sprite(position.x, position.y, 'tileWALL_SIDE');
-                position = KodingSpy.Utils.getWorldPosition(columns, y);
-                var rightSprite = this.game.add.sprite(position.x, position.y, 'tileWALL_SIDE');
-                rightSprite.scale.x = -1;
+            for (var y = 0; y < rows-1; y++) {
+                this.placeTileAt(0, y, 'tileWALL_SIDE');
+                this.placeTileAt(columns, y, 'tileWALL_SIDE').scale.x = -1;
             }
+
+            // Corner tiles
+            this.placeTileAt(0, rows-1, 'tileWALL_CORNER');
+            this.placeTileAt(columns, rows-1, 'tileWALL_CORNER').scale.x = -1;
+            // var position = KodingSpy.Utils.getWorldPosition(0, rows-1);
+            // this.game.add.sprite(position.x, position.y, 'tileWALL_SIDE');
+            // var position = KodingSpy.Utils.getWorldPosition(columns-1, rows-1);
+            // this.game.add.sprite(position.x, position.y, 'tileWALL_SIDE');
+        }
+
+        placeTileAt(x: number, y: number, tile :String): Phaser.Sprite {
+            var position = KodingSpy.Utils.getWorldPosition(x, y);
+            return this.game.add.sprite(position.x, position.y, tile);
         }
     }
 }
