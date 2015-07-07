ddb19063c54dac3a10d7586d652b6b9a95c6ab00
Edgar Neto <edgar.figueiredo@gmail.com>
2015-07-07 23:55:02 +0200

    Exported TileSets. Re-made level 2

---
+++ lucy/dev/game/assets/levels/Tilesets/door.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="door" tilewidth="50" tileheight="50" spacing="1">
+ <image source="../../tiles/door.png" width="50" height="101"/>
+ <tile id="1">
+  <properties>
+   <property name="frames" value="1"/>
+   <property name="type" value="door"/>
+  </properties>
+ </tile>
+</tileset>
